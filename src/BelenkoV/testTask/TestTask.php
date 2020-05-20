c4f7ad7dd51170b1e83c1d866d164b916b9e736b
BelenkoValery <65164103+BelenkoValery@users.noreply.github.com>
2020-05-21 00:34:41 +0400

    Create TestTask.php

---
+++ src/BelenkoV/testTask/TestTask.php
@@ -0,0 +1,23 @@
+<?
+/**
+ * Created by BelenkoVS.
+ * date 20/05/2020
+ * time 23:00.
+ */
+
+namespace belenkov\testTask
+
+/**
+ * Class TestTask
+ * @package App\TestTask
+ *
+ * @Route(path="/testTask")
+ */
+ 
+class TestTask
+{
+    private $customerRepository;
+
+}
+
+?>
